627c128dba03eee29c8a1ab9ff93ba94f34dd2af
Boussari Latif <latif.boussari@mehriso.com>
2019-09-09 21:29:19 +0100

    refactoring row widget to listtile

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
           body: SafeArea(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 CircleAvatar(
                   backgroundImage: NetworkImage(
@@ -37,15 +38,36 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 2.5
                   ),
                 ),
-                Container(
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.phone,
-                        color: Colors.white,
-                      )
-                    ],
+                SizedBox(height: 10.0,),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                  // padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color:Colors.teal.shade900
+                    ),
+                    title: Text(
+                         '+229 97731064',
+                           style:TextStyle(
+                           color: Colors.teal.shade900,
+                           fontSize: 20.0
+                       )
+                     ),
                   ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                  // padding: EdgeInsets.all(10.0),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text('boussarilatif1996@gmail.com'),
+                  )
                 )
 
               ],
@@ -54,3 +76,37 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// Row(
+//                     children: <Widget>[
+//                       Icon(
+//                         Icons.phone,
+//                         color: Colors.teal.shade900,
+//                       ),
+//                       SizedBox(width: 9.0,),
+//                       Text(
+//                         '+229 97731064',
+//                         style:TextStyle(
+//                           color: Colors.teal.shade900,
+//                           fontSize: 20.0
+//                         )
+//                       )
+//                     ],
+//                   )
+
+// Row(
+//                     children: <Widget>[
+//                       Icon(
+//                         Icons.email,
+//                         color: Colors.teal.shade900,
+//                       ),
+//                       SizedBox(width: 9.0,),
+//                       Text(
+//                         'boussarilatif1996@gmail.com',
+//                         style:TextStyle(
+//                           color: Colors.teal.shade900,
+//                           fontSize: 20.0
+//                         )
+//                       )
+//                     ],
+//                   )
